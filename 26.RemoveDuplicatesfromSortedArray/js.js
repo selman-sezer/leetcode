87c6d89a8423e80d7fd989ef5f23641376be752e
selmansezer <selmansezer.cs@gmail.com>
2024-12-24 18:29:49 +0300

    Solve the Remove Duplicates from Sorted Array (26)

---
+++ 26.RemoveDuplicatesfromSortedArray/js.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let unique = 0;
+    let numsMap = new Map();
+    for(const num of nums )
+    {
+        if(! numsMap.has(num))
+        {
+            nums[unique] =num;
+            unique += 1;
+            numsMap.set(num, true);
+        }
+    }
+    return unique;
+};
+
