676b270a5a6e2417856284099f8c45c1033543fa
selmansezer <selmansezer.cs@gmail.com>
2024-12-27 00:30:19 +0300

    Improve the execution time for Longest Palindromic Substring (5) problem

--- 5. Longest Palindromic Substring/LongestPalindromicSubstring.js
+++ 5. Longest Palindromic Substring/LongestPalindromicSubstring.js
@@ -12,27 +12,25 @@ var longestPalindrome = function(s) {
         end = diff;
         let left = 0;
         while (end < len) {
-            pairs.push([left, left+diff]);
+            if (s[left] == s[left+diff]) {
+                pairs.push([left, left+diff]);     
+            }
             left+=1;
             end +=1;
         }
         diff -= 1;
     }
-
-    let notPalSet = new Set();
     let palSet = new Set();
     let currentPair;
 
     while (pairs.length > 0) {
         currentPair = pairs.pop();
-            if (s[currentPair[0]] == s[currentPair[1]] && (! notPalSet.has(`${[currentPair[0]+1, currentPair[1]-1]}`))) {
+        if ((currentPair[1] - currentPair[0])  < 3) {
             palSet.add(`${currentPair}`);
         }
-        else
-        {
-            notPalSet.add(`${currentPair}`);
+        else if ( (currentPair[1] - currentPair[0])  >= 3 && (palSet.has(`${[currentPair[0]+1, currentPair[1]-1]}`))) {
+            palSet.add(`${currentPair}`);
         }
-
     }
 
     let max = 0;
