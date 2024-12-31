5d8c5d6a7a748e0204b5dacc35a53dba0b356e53
selmansezer <selmansezer.cs@gmail.com>
2024-12-31 10:06:08 +0300

    Solve the Letter Combinations of a Phone Number (17) problem

---
+++ 17. Letter Combinations of a Phone Number/LetterCombinationsofaPhoneNumber.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    if (digits.length == 0) {
+        return []
+    }
+    let digitsToLettersMap = new Map();
+
+    digitsToLettersMap.set(2, 'abc');
+    digitsToLettersMap.set(3, 'def');
+    digitsToLettersMap.set(4, 'ghi');
+    digitsToLettersMap.set(5, 'jkl');
+    digitsToLettersMap.set(6, 'mno');
+    digitsToLettersMap.set(7, 'pqrs');
+    digitsToLettersMap.set(8, 'tuv');
+    digitsToLettersMap.set(9, 'wxyz');
+
+    let stringLength = digits.length;
+    let currentCharacterIndex = 0;
+    let result = [''];
+    for(let digit of digits) {
+        let tempList= [];
+        while (result.length != 0) {
+            let tempString = result.pop();
+            let letters = digitsToLettersMap.get(+digit);
+            for(const char of letters)
+            {
+                tempList.push(tempString+char);
+            }
+        }
+        result = tempList;
+    }
+
+    
+    return result;
+
+};
+
+
+
+let digits = "456";
+console.log(letterCombinations(digits));
